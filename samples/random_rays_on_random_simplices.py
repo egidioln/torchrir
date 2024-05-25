00773b0c51428f6888bc156d21ffe48d9eaca1dc
Lucas Egidio <lucasegidio1@gmail.com>
2024-05-25 21:04:18 +0200

    ADD: Mirror point on patch plane

--- samples/random_rays_on_random_simplices.py
+++ samples/random_rays_on_random_simplices.py
@@ -3,49 +3,52 @@ import torch
 from torchrir.geometry import Patch, Ray
 
 _DV = torch.device("cuda")
+_N = 5_000_000
+N_BENCHMARK = 100
 
-N_RAYS = 10_000_000
 
-
-def benchmark_random_rays_on_1_simplice():
+def benchmark_random_rays_on_random_simplices():
     results = dict()
 
-    p = Patch(torch.eye(3, device=_DV))
 
     def benchmark_sequential():
         hit = 0.0
-        for _ in range(N_RAYS):
-            hit += Ray(torch.randn(3, device=_DV)).intersects(p)[0]
+        for _ in range(_N):
+            p = Patch(torch.randn(3, 3, device=_DV))
+            hit += Ray(torch.randn(3, device=_DV)).intersects(p)
 
-        if abs(hit / N_RAYS - 0.125) > 1e-1:
+        if abs(hit / _N - 0.125) > 1e-1:
             raise AssertionError(
                 "Flaky assertion failed; running benchmark again may solve it, "
                 "otherwise check installation integrity and that N_RAYS is 10_000_000."
             )
 
-        print(hit / N_RAYS)
+        print(hit / _N)
 
 
+    p = Patch(torch.randn(_N, 3, 3, device=_DV))
+    ray = Ray(torch.randn(_N, 3, device=_DV))
     def benchmark_broadcast():
-        hit = Ray(torch.randn(N_RAYS, 3, device=_DV)).intersects(p)[0].sum().item()
-
-        if abs(hit / N_RAYS - 0.125) > 1e-1:
+        hit = ray.intersects(p)[0].sum().item()
+        if abs(hit / _N - 0.125) > 1e-1:
             raise AssertionError(
                 "Flaky assertion failed; running benchmark again may solve it, "
                 "otherwise check installation integrity and that N_RAYS is 10_000_000."
             )
 
-        print(hit / N_RAYS)
+        print(hit / _N)
 
     for benchmark in [
         benchmark_broadcast,
+        # torch.compile(benchmark_broadcast),
         # benchmark_sequential,
     ]:
         # benchmark_sequential = torch.compile(benchmark_sequential)
         benchmark()
-        results[benchmark] = timeit.timeit(benchmark, number=2)
+        
+        results[benchmark] = timeit.timeit(benchmark, number=N_BENCHMARK) / N_BENCHMARK
         print(benchmark.__name__, results[benchmark])
 
 
 if __name__ == "__main__":
-    benchmark_random_rays_on_1_simplice()
+    benchmark_random_rays_on_random_simplices()
--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -26,13 +26,21 @@ def test_patch_is_planar():
 
 
 def test_ray_intersects_convex_patch():
-    p = Patch(vertices=torch.eye(3)[None, ...])
+    patch = Patch(vertices=torch.eye(3)[None, ...])
     
-    assert Ray(torch.ones(1, 3)).intersects(p)
-    assert Ray(torch.tensor([1., 0., 0.])).intersects(p)
-    
-    assert not Ray(torch.tensor([-1., 1., 1.])).intersects(p)
-    assert not Ray(-torch.ones(3)).intersects(p)
+    intersects, p0 = Ray(torch.ones(1, 3)).intersects(patch)
+    assert intersects
+    assert torch.allclose(p0, torch.ones(1, 3)/3)
+    intersects, p0 = Ray(torch.tensor([1., 0., 0.])).intersects(patch)
+    assert torch.allclose(p0, torch.tensor([1., 0., 0.]))
+    assert intersects
+
+    intersects, p0 = Ray(torch.tensor([-1., 1., 1.])).intersects(patch)
+    assert not intersects
+
+    intersects, p0 = Ray(-torch.ones(3)).intersects(patch)
+    assert not intersects
+
 
 
 def test_ray_intersects_convex_patch_broadcasting():
@@ -46,6 +54,47 @@ def test_ray_intersects_convex_patch_broadcasting():
 
         rays = Ray(*torch.randn(2, n_batch, 3))
 
-        hits += rays.intersects(patches).sum() / n_batch / n_reps
+        hits += rays.intersects(patches)[0].sum() / n_batch / n_reps
 
     assert  0.121 < hits < 0.124
+
+def test_mirror_point_on_patch():
+    patch = Patch(torch.eye(3))
+
+    p = torch.zeros(3)
+    assert torch.allclose(patch.mirror(p), 2 * torch.ones(3)/3)
+
+    p = 2 * torch.ones(3) / 3
+    assert torch.allclose(patch.mirror(p), torch.zeros(3), atol=1e-7)
+
+    patch = Patch(torch.tensor(
+        [
+            [1., 4., 0.],
+            [1., 4., 1.],
+            [0., 4., 1.],
+        ]
+    ))
+
+    p = 2 * torch.ones(3) / 3
+    assert torch.allclose(patch.mirror(p), torch.tensor([p[0], 8-p[1], p[2]]), atol=1e-7)
+
+
+
+def test_mirror_point_on_patch_broadcasting():
+    n_batches = 1000
+    patch = Patch(torch.randn(n_batches, 3, 3))
+    p = torch.randn(n_batches, 3)
+    p_rr = patch.mirror(patch.mirror(p))
+    assert torch.allclose(p, p_rr.squeeze(), atol=1e-6)
+
+    n_batches = 1000
+    patch = Patch(torch.randn(n_batches, 3, 3))
+    p = torch.randn(3)
+    p_rr = patch.mirror(patch.mirror(p))
+    assert torch.allclose(p, p_rr.squeeze(), atol=1e-5)
+
+    n_batches = 1000
+    patch = Patch(torch.randn(3, 3))
+    p = torch.randn(n_batches, 3)
+    p_rr = patch.mirror(patch.mirror(p))
+    assert torch.allclose(p, p_rr.squeeze(), atol=1e-6)

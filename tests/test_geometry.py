fix: clearer broadcast of patches in contains()

tests/test_geometry.py
@@ -96,13 +96,12 @@ def test_mirror_point_on_patch():
 
 
 def test_mirror_point_on_patch_broadcasting():
-    n_batches_a = 10
-    n_batches_b = 9
-    s = Source(torch.randn(n_batches_a, n_batches_a, 3, 1))
-    patch = Patch(torch.randn(n_batches_b, n_batches_b, 3, 3))
+    n_batches = 3
+    s = Source(torch.randn(n_batches, n_batches, 3, 1))
+    patch = Patch(torch.randn(3, 3))
     p_r = patch._mirror(s, force_product=True)
-    assert p_r.p.shape == (n_batches_a, n_batches_a, n_batches_b, n_batches_b, 3, 1)
-    assert p_r.intensity.shape == (n_batches_a, n_batches_a, n_batches_b, n_batches_b)
+    assert p_r.p.shape == (n_batches, n_batches, 3, 1)
+    assert p_r.intensity.shape == (n_batches, n_batches)
 
     n_batches = 100
     s = Source(torch.randn(n_batches, 3, 1))

torchrir/geometry.py
@@ -244,14 +244,22 @@ class Patch:
         Returns:
             bool: true iff p in self
         """
+
+        def broadcast_mask_if_needed(x: Tensor) -> Tensor:
+            if x.ndim > 2:
+                return x[mask]
+            if x.ndim == 2:  # allow
+                return x.broadcast_to((torch.sum(mask).item(), *x.shape))
+            return x
+
         rel_vertices = self._rel_vertices
         origin = self.origin
         if mask is not None:
             if not mask.any():
                 return torch.full_like(mask, False, dtype=torch.bool)
             p = p[mask]
-            rel_vertices = rel_vertices[mask]
-            origin = origin[mask]
+            rel_vertices = broadcast_mask_if_needed(rel_vertices)
+            origin = broadcast_mask_if_needed(origin)
 
         def _circle_pairwise(x: Tensor):
             x = rearrange(x, "... d n -> n ... d 1")